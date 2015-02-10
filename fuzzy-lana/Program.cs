9cb87058b5377d8d00a6d2d5c848842127668cc3
LordOfTheDivX <viper6437@gmail.com>
2015-02-10 18:11:17 -0500

    Blah

--- fuzzy-lana/Program.cs
+++ fuzzy-lana/Program.cs
@@ -13,15 +13,19 @@ namespace fuzzy_lana
             //Hello World Segment
             Console.WriteLine("Hello World!");
             Console.ReadKey();
+                        
+            Console.WriteLine("What's the Password?\n");
+            string saul=Console.ReadLine();
+            
+            if(saul=="ILoveBoobs" || saul=="3wa4es5rd6tf*"){
+                /*Pinging Google via prompted CLI
+                Wanting to do these commands without the popup.
+                Ideally the program will run behind the scenes and under the radar.*/
+                //string strCmdText;
+                string strCmdText = "/C ping google.com";
+                System.Diagnostics.Process.Start("CMD.exe", strCmdText);
+                Console.ReadKey(); }
 
-            /*Pinging Google via prompted CLI
-             Wanting to do these commands without the popup.
-             Ideally the program will run behind the scenes and under the radar.*/
-            string strCmdText;
-            strCmdText = "/C ping google.com";
-            System.Diagnostics.Process.Start("CMD.exe", strCmdText);
-
-            Console.ReadKey();
         }
     }
 }
