55b6a6e8e8f59c355e6fb539ec1f13e112f5a066
LordOfTheDivX <viper6437@gmail.com>
2015-02-10 11:59:26 -0500

    Commit

---
+++ fuzzy-lana/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fuzzy_lana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
