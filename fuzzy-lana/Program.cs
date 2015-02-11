d6d7e3bce7857379bd18bf14865c1010aec2e319
LordOfTheDivX <viper6437@gmail.com>
2015-02-11 09:13:02 -0500

    Server line is unclear. How to input in cmd without an argument?

--- fuzzy-lana/Program.cs
+++ fuzzy-lana/Program.cs
@@ -24,24 +24,24 @@ namespace fuzzy_lana{
 
         //SPAAAAAAAACCCEEEE
         static void CLI_CLEAN(){
-        //Delete Temp Files, supress errors - FINISHED
-        System.Diagnostics.Process Clean=new System.Diagnostics.Process();
-        System.Diagnostics.ProcessStartInfo GitRDone=new System.Diagnostics.ProcessStartInfo();
-        GitRDone.WindowStyle=System.Diagnostics.ProcessWindowStyle.Hidden;
-        GitRDone.FileName="cmd.exe";
-        GitRDone.Arguments = "/C del %homedrive%\\Temp\\*.* /F /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
-        GitRDone.Arguments = "/C RMDIR %homedrive%\\Temp\\ /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
-        GitRDone.Arguments = "/C del %localappdata%\\Temp\\*.* /F /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
-        GitRDone.Arguments = "/C RMDIR %localappdata%\\Temp\\ /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
-        GitRDone.Arguments = "/C del %appdata%\\Temp\\*.* /F /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
-        GitRDone.Arguments = "/C RMDIR %appdata%\\Temp\\ /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
-        GitRDone.Arguments = "/C del %Temp%\\*.* /F /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
-        GitRDone.Arguments = "/C RMDIR %Temp%\\ /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
+            //Delete Temp Files, supress errors - FINISHED
+            System.Diagnostics.Process Clean=new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo GitRDone=new System.Diagnostics.ProcessStartInfo();
+            GitRDone.WindowStyle=System.Diagnostics.ProcessWindowStyle.Hidden;
+            GitRDone.FileName="cmd.exe";
+            GitRDone.Arguments = "/C del %homedrive%\\Temp\\*.* /F /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
+            GitRDone.Arguments = "/C RMDIR %homedrive%\\Temp\\ /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
+            GitRDone.Arguments = "/C del %localappdata%\\Temp\\*.* /F /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
+            GitRDone.Arguments = "/C RMDIR %localappdata%\\Temp\\ /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
+            GitRDone.Arguments = "/C del %appdata%\\Temp\\*.* /F /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
+            GitRDone.Arguments = "/C RMDIR %appdata%\\Temp\\ /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
+            GitRDone.Arguments = "/C del %Temp%\\*.* /F /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
+            GitRDone.Arguments = "/C RMDIR %Temp%\\ /S /Q"; Clean.StartInfo = GitRDone; Clean.Start();
         }
 
         //SPAAAAAAAACCCEEEE
         static void PROG_CLEAN(){
-        //Check for %LOCALAPPDATA%\JSM - If not exist, fetch package and extract
+            //Check for %LOCALAPPDATA%\JSM - If not exist, fetch package and extract
 
 
             //Place an IF statement so that files are in JSM folder
@@ -57,10 +57,88 @@ namespace fuzzy_lana{
         }
 
         //SPAAAAAAAACCCEEEE
-        static void BATCH_DUMP(){
-            //Use selector above and place code down here
-            //I want it to dump VBS and batch scripts that I use regularly
+        static void Exchange_2013_RUNNING(){
+            //Check for running MS Exchange 2013 Processes
+            System.Diagnostics.Process Check=new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo CheckThis=new System.Diagnostics.ProcessStartInfo();
+            CheckThis.WindowStyle=System.Diagnostics.ProcessWindowStyle.Hidden;
+            CheckThis.FileName="cmd.exe";
 
+            
+            CheckThis.Arguments = "sc query MSExchangeADTopology \| find \"STATE\" \| find \"RUNNING\"\rif .%errorlevel%.==.1. sc start \"MSExchangeADTopology\""; Check.StartInfo=CheckThis; CheckThis.Start();
+            
+            sc query MSExchangeAntispamUpdate | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeAntispamUpdate"
+            
+            sc query MSExchangeDagMgmt | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeDagMgmt"
+
+            sc query MSExchangeDiagnostics | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeDiagnostics"
+
+            sc query MSExchangeEdgeSync | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeEdgeSync"
+
+            sc query MSExchangeFrontEndTransport | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeFrontEndTransport"
+
+            sc query MSExchangeHM | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeHM"
+
+            sc query MSExchangeIS | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeIS"
+
+            sc query MSExchangeMailboxAssistants | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeMailboxAssistants"
+
+            sc query MSExchangeMailboxReplication | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeMailboxReplication"
+
+            sc query MSExchangeDelivery | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeDelivery"
+
+            sc query MSExchangeSubmission | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeSubmission"
+
+            sc query MSExchangeMigrationWorkflow | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeMigrationWorkflow"
+
+            sc query MSExchangeRepl | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeRepl"
+
+            sc query MSExchangeRPC | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeRPC"
+
+            sc query MSExchangeFastSearch | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeFastSearch"
+
+            sc query HostControllerService | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "HostControllerService"
+
+            sc query MSExchangeServiceHost | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeServiceHost"
+
+            sc query MSExchangeThrottling | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeThrottling"
+
+            sc query MSExchangeTransport | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeTransport"
+
+            sc query MSExchangeTransportLogSearch | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeTransportLogSearch"
+
+            sc query MSExchangeUM | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeUM"
+
+            sc query MSExchangeUMCR | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "MSExchangeUMCR"
+
+            sc query FMS | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "FMS"
+
+            sc query msoidsvc | find "STATE" | find "RUNNING"
+            if .%errorlevel%.==.1. sc start "msoidsvc"
+            
         }
     }
 }
